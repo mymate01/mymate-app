935f4e0d0cec48bfb3db1c0ae62afb29c333accd
Parameswara Malla <pmalla@intuceo.com>
2026-05-23 16:58:43 +0530

    style(MYM-11): stack short tiles in one column and reduce node height to save vertical space

--- src/components/CareerMap/CourseInfoPanel.tsx
+++ src/components/CareerMap/CourseInfoPanel.tsx
@@ -20,17 +20,23 @@ export default function CourseInfoPanel({ details, title }: CourseInfoPanelProps
           </div>
         )}
 
-        {details.duration && (
-          <div className={`${styles.tile} ${styles.smallTile}`}>
-            <h4>Duration</h4>
-            <div className={styles.valueLarge}>{details.duration}</div>
-          </div>
-        )}
-
-        {details.fees && (
-          <div className={`${styles.tile} ${styles.smallTile}`}>
-            <h4>Approx. Fees</h4>
-            <div className={styles.valueLarge}>{details.fees}</div>
+        {(details.duration || details.fees) && (
+          <div className={`${styles.tile} ${styles.stackedTile}`}>
+            {details.duration && (
+              <div className={styles.stackItem}>
+                <h4>Duration</h4>
+                <div className={styles.valueLarge}>{details.duration}</div>
+              </div>
+            )}
+            
+            {details.duration && details.fees && <div className={styles.divider} />}
+            
+            {details.fees && (
+              <div className={styles.stackItem}>
+                <h4>Approx. Fees</h4>
+                <div className={styles.valueLarge}>{details.fees}</div>
+              </div>
+            )}
           </div>
         )}
 
